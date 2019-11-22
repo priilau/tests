c1eebde91a0a8c2d557077ba18b393120a1346ec
plaupa <priit.laupa@gmail.com>
2019-11-22 10:23:59 +0200

    Test 8 tehtud

--- src/Test8.jsx
+++ src/Test8.jsx
@@ -1,11 +1,64 @@
 import React from "react";
 
 class Test8 extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.enabled = true;
+  }
+
+  state = {
+    fullName: "",
+    phoneNumber: 0,
+    address: "",
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    fetch("/api/v1/users/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(this.state)
+    })
+    .then(res => res.text())
+    .then(data => {
+      console.log(data);
+      this.setState({responseText: data});
+    })
+    .catch(err => {
+      console.log("err:", err);
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name] : e.target.value,
+    });
+  }
+
+  enableDisable = () => {
+    if(this.enabled) {
+      this.enabled = false;
+    } else {
+      this.enabled = true;
+    }
+    document.getElementById("fieldset").disabled = this.enabled;
+  }
+
   render() {
     return (
       <div>
         <Task />
-        implement
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <fieldset id="fieldset">
+            <input name="fullName" onChange={this.handleChange} placeholder="full name" value={this.state.fullName} />
+            <input name="phoneNumber" onChange={this.handleChange} placeholder="phone number" value={this.state.phoneNumber} />
+            <input name="address" onChange={this.handleChange} placeholder="address" value={this.state.address} />
+            <input type="submit" value="Send data"/>
+          </fieldset>
+        </form>
+        <button onClick={this.enableDisable}>enable/disable</button>
       </div>
     );
   }
