2dec3a7248071639a1f891f34ba2685323629e40
plaupa <priit.laupa@gmail.com>
2019-11-29 10:36:41 +0200

    Yl 10 tehtud

--- src/tests/Test10.jsx
+++ src/tests/Test10.jsx
@@ -1,11 +1,51 @@
 import React from "react";
 
 class Test10 extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.fullName = React.createRef();
+    this.personalCode = React.createRef();
+    this.phoneNumber = React.createRef();
+    this.address = React.createRef();
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const formBody = {
+     fullName: this.fullName.current.value,
+     personalCode: this.personalCode.current.value,
+     phoneNumber: this.phoneNumber.current.value,
+     address: this.address.current.value,
+    };
+    fetch("/api/v1/users/add", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(formBody)
+    })
+    .then(res => res.text())
+    .then(data => {
+      console.log(data);
+      this.setState({responseText: data});
+    })
+    .catch(err => {
+      console.log("err:", err);
+    });
+  }
+
   render() {
     return (
       <div>
         <Task />
-        implement
+        <form onSubmit={this.handleSubmit.bind(this)}>
+          <input name="fullName" placeholder="full name" ref={this.fullName}/>
+          <input name="personalCode" placeholder="personal code" ref={this.personalCode}/>
+          <input name="phoneNumber" placeholder="phone number" ref={this.phoneNumber}/>
+          <input name="address" placeholder="address" ref={this.address}/>
+          <input type="submit" value="Send data"/>
+        </form>
       </div>
     );
   }
